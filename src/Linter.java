c1c6c2af4ae5f25717cadebbb38a168ab0b56fc0
Kyle <Kyle.rogers533@gmail.com>
2015-09-04 16:34:57 -0500

    Linter source and class files for the JavaScript linter.

---
+++ src/Linter.java
@@ -0,0 +1,43 @@
+import java.lang.*;
+import java.io.*;
+
+public class Linter {
+
+  // Trims all trailing whitespace and checks if the line in the file contains
+  // a {, }, or is empty. If the line doesn't contain these then it checks if
+  // the line contains a semicolon, if it does the method returns false.
+  public static boolean checkLine(String line) {
+    line = line.trim();
+    if (line.contains("}") || line.contains("{") || line.length() == 0) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  // Main method, opens the file and iterates over each line and uses the
+  // checkLine method to verify that each line, that doesn't contain loops,
+  // functions, if blocks, or while blocks, ends with a semicolon
+  public static void main(String [] args) {
+    String line = null;
+    try {
+      FileReader fileReader = new FileReader(args[0]);
+
+      BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+      int lineNum = 1;
+
+      while ((line = bufferedReader.readLine()) != null) {
+        if (checkLine(line)) {
+          System.out.println(lineNum + ". Statement should end with a semicolon");
+        }
+        lineNum++;
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("Caught FileNotFoundException: " + e.getMessage());
+    } catch (IOException e) {
+      System.out.println("Caught IOException: " + e.getMessage());
+    }
+  }
+}
