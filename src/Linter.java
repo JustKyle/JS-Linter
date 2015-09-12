5b4926a6d87a351fbb12c7281e467715eb5dd6ca
Kyle <Kyle.rogers533@gmail.com>
2015-09-12 15:02:18 -0500

    Minor code fixes

--- src/Linter.java
+++ src/Linter.java
@@ -7,14 +7,15 @@ public class Linter {
   // a {, }, or is empty. If the line doesn't contain these then it checks if
   // the line contains a semicolon, if it does the method returns false.
   public static boolean checkLine(String line) {
-    line = line.trim();
-    if (line.contains("}") || line.contains("{") || line.length() == 0) {
+    if (line.trim().endsWith("}") || line.trim().endsWith("{") || line.length() == 0) {
       return false;
     }
-    else if(line.substring(line.lenght - 1) != ";") {
+    else if(line.trim().endsWith(";")) {
+      return false;
+    }
+    else {
       return true;
     }
-    return true;
   }
 
   // Main method, opens the file and iterates over each line and uses the
@@ -35,9 +36,12 @@ public class Linter {
         }
         lineNum++;
       }
-    } catch (FileNotFoundException e) {
+      bufferedReader.close();
+    }
+    catch (FileNotFoundException e) {
       System.out.println("Caught FileNotFoundException: " + e.getMessage());
-    } catch (IOException e) {
+    }
+    catch (IOException e) {
       System.out.println("Caught IOException: " + e.getMessage());
     }
   }
